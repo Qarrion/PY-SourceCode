0816ebb27db56fd3f00c7e971cc9058ea2cf0d12
qarrion <qarrion.k@gmail.com>
2021-10-03 20:03:59 +0900

    logging  components2

--- Qlogging/component.py
+++ Qlogging/component.py
@@ -38,3 +38,18 @@ root_logger.warning("test")
 root_logger.error("test")
 
 # %%
+
+import logging
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler("file.log")
+file_handler.setFormatter(formatter)
+
+root_logger = logging.getLogger()
+root_logger.addHandler(console_handler)
+root_logger.addHandler(file_handler)
+root_logger.setLevel(logging.WARNING)
+root_logger.warning("test")
